5fbc84a361d58ff81aab3bb64c0a676773059f30
Kavinduherarth_github <151900001+kavindugit@users.noreply.github.com>
2024-12-11 15:50:42 +0530

    update6

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,7 +1,14 @@
+import React , {useState} from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import {useNavigate , useParams } from 
+
 const DeleteBook = () => {
     return(
         <div>DeleteBook</div>
-    )
+    );
 }
+
 
 export default DeleteBook;
