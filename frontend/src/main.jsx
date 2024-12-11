a41c395ae45068ab7ee8455803a157a6d71833f8
Kavinduherarth_github <151900001+kavindugit@users.noreply.github.com>
2024-12-12 04:24:24 +0530

    update8

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
 
 //const App =() =><h1>Hellow KAvindu</h1>;
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <SnackbarProvider>
+      <App />
+    </SnackbarProvider>
   </BrowserRouter>
 );
--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -3,6 +3,7 @@ import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 import axios from "axios";
 import { useNavigate } from "react-router-dom"; 
+import {useSnackbar} from 'notistack';
 
 const CreateBook = () => {
     const [title, setTitle] = useState("");
@@ -10,7 +11,7 @@ const CreateBook = () => {
     const [publishYear, setPublishYear] = useState("");
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
-
+    const {enqueueSnackbar} = useSnackbar();
     const handleSaveBook = () => {
         const data ={
             title,
@@ -22,11 +23,12 @@ const CreateBook = () => {
             .post("http://localhost:5555/books", data)
             .then(() => {
                 setLoading(false);
+                enqueueSnackbar('Book Created succesfully',{varient :'success'});
                 navigate("/");
             })
             .catch((error) => {
                 setLoading(false);
-                alert("An error occurred. Please try again");
+                enqueueSnackbar('Error' , {varient:'error'});
             });
     }
     return(
